52afd61f6826be8baefbb0333ff55cecf27a8d20
MadEyE355 <moksh9355472532@gmail.com>
2026-02-14 01:43:03 +0530

    completed custom xss (dom injector with custom payload type)

--- popup.js
+++ popup.js
@@ -6,14 +6,13 @@ const customXssStartBtn = document.getElementById("custom_xss_start_btn");
 const customXssPayload = document.getElementById("custom_xss_payload");
 const customXssIS = document.getElementById("custom_xss_input_selector");
 // IS = input selector
-const customXssSBS = document.getElementById("custom_xss_searchBtn_selector") 
+const customXssSBS = document.getElementById("custom_xss_searchBtn_selector")
 // SBS = searchBtn selector
 const xssStartBtns = document.querySelectorAll(".xss_start_btns");
+const checkLastPayloadBtn = document.getElementById("check_last_payload");
 
-xssStartBtns.forEach((btn)=>{
-    btn.addEventListener("click", ()=>{
-        btn.style.display = "none";
-    })
+checkLastPayloadBtn.addEventListener("click", () => {
+    get_last_tested_payload()
 })
 
 
@@ -27,32 +26,100 @@ customXssBtn.addEventListener("click", (e) => {
     customXssStartBtn.style.display = "block";
 })
 
+xssStartBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        btn.style.display = "none";
+    })
+})
 
-customXssStartBtn.addEventListener("click", async (e) => {
 
+customXssStartBtn.addEventListener(("click") , async (e) => {
     const payload = customXssPayload.value;
     const attackSite_CXss = customXssIS.value;
     const searchBtn = customXssSBS.value;
 
-    if(!payload.trim() || !attackSite_CXss.trim() || !searchBtn.trim()) {
-        alert("fill all values") 
+    if (!payload.trim() || !attackSite_CXss.trim() || !searchBtn.trim()) {
+        alert("fill all values")
         return;
     }
 
-        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-        chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        args: [payload , attackSite_CXss , searchBtn],
-
-        func: (param_payload , param_attackSite , param_searchBtn) => {
-            var parts_payload = param_payload.split("\n")
-            console.log(parts_payload);
-
-
-        }
-
-        })
+    await start_xss(payload , attackSite_CXss , searchBtn)
 
 
 })
+
+async function start_xss(param_payload , param_attackSite , param_searchBtn) {
+        let payload_parts = param_payload.split("\n");
+
+
+        if(Number(sessionStorage.getItem("indx")) >= payload_parts.length){
+            return;
+        }
+        else
+        {
+            let index = Number(sessionStorage.getItem("indx") || 0)
+            sessionStorage.setItem("indx" , index+1)
+
+            payload_to_test = payload_parts[index]
+            
+            await set_last_tested_payload(payload_to_test)
+
+            
+            const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+            chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            args: [payload_to_test, param_attackSite, param_searchBtn],
+    
+                func: async (payload_to_test, param_attackSite, param_searchBtn) => {
+                    
+                    console.log('testing : ' , payload_to_test)
+
+                    const input_box = document.querySelector(param_attackSite);
+                    const search_btn = document.querySelector(param_searchBtn);
+
+                    if (!input_box || !search_btn) {
+                    console.log("[XSS] Selector not found");
+                    return;
+                    }
+
+                    input_box.value = payload_to_test;
+                    input_box.dispatchEvent(new Event("input", { bubbles: true }));
+
+                    search_btn.click()
+
+
+                } 
+
+            })
+
+            await waitForTabLoad(tab.id)
+
+            await start_xss(param_payload , param_attackSite, param_searchBtn)
+        }
+}
+
+async function waitForTabLoad(tabId) {
+    return new Promise((resolve) => {
+        chrome.tabs.onUpdated.addListener(function listener(updatedTabId, info) {
+            if (updatedTabId === tabId && info.status === "complete") {
+                chrome.tabs.onUpdated.removeListener(listener);
+                resolve();
+            }
+        });
+    });
+}
+
+
+
+async function set_last_tested_payload(p){
+    await chrome.storage.local.set({
+  last_payload_tested: p
+});
+}
+
+async function get_last_tested_payload(){
+const { last_payload_tested } = await chrome.storage.local.get("last_payload_tested");
+alert(last_payload_tested);
+}
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log(Number('l') > 4)
